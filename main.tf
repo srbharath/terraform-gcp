2579c8d08b0958243c1220c3faf7b03fb606b5b7
srbharath <103111244+srbharath@users.noreply.github.com>
2023-09-16 00:57:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,24 @@ resource "google_cloud_run_service" "example" {
     }
   }
 }
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"  # Adjust the machine type as needed
+  zone         = "us-central1-a"  # Adjust the zone as needed
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"  # Adjust the image as needed
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
 # Output the URL of the Cloud Run service
 output "service_url" {
   value = google_cloud_run_service.example.status[0].url
