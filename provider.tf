74586ce3b5d39b95069566cfb96a2871c3b15482
srbharath <103111244+srbharath@users.noreply.github.com>
2023-09-15 23:18:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,5 @@
-provider "google" {
-  project = "imposing-voyage-392509"
-}
-
-terraform {
-	required_providers {
-		google = {
-	    version = "~> 4.60.0"
-		}
-  }
+provider "google"{
+project = "imposing-voyage-392509"
+region = "us-central1"
+credentials = "/mnt/workspace/keys.json"
 }
